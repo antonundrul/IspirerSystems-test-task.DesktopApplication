f76453d493e0e8b0238cccd264c0f5fe17c619e0
Anton Undrul <antonundrul@gmail.com>
2022-07-20 22:29:18 +0300

    implemented a custom List ListUsr and User class that counts its objects

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,32 +1,85 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class Controller {
-    @FXML
-    private Button showInfoButton;
-    @FXML
-    private MenuButton languagesMenuButton;
-    @FXML
-    private MenuItem russianMenuItem;
-    @FXML
-    private MenuItem englishMenuItem;
-    @FXML
-    private Label languageLabel;
-    @FXML
-    private Label taskLabel;
+
+    //task1
+    @FXML private TextField nameTextField;
+    @FXML private TextField ageTextField;
+    @FXML private Button addUserButton;
+    @FXML private TextField usersNumberTextField;
+
+    //task2
+    @FXML private TableView<User> usersTableView;
+    @FXML private TableColumn<User, String> nameColumn;
+    @FXML private TableColumn<User, String> ageColumn;
+    @FXML private TextField listSizeTextField;
+    @FXML private TextField findUserByNumberTextField;
+    @FXML private Button findUserButton;
+    @FXML private TextField foundNameTextField;
+    @FXML private TextField foundAgeTextField;
+
+    //task3
+    @FXML private Button showInfoButton;
+    @FXML private MenuButton languagesMenuButton;
+    @FXML private MenuItem russianMenuItem;
+    @FXML private MenuItem englishMenuItem;
+    @FXML private Label languageLabel;
+    @FXML private Label taskLabel;
 
     private Language currentLanguage = Language.RUSSIAN;
+    private ListUsr<User> users = new ListUsr<>();
+
+    public void addUserButtonClick(){
+       String name = nameTextField.getText();
+       int age = Integer.parseInt(ageTextField.getText());
+
+       User newUser = new User(name,age);
+
+       nameTextField.setText("");
+       ageTextField.setText("");
+       usersNumberTextField.setText(String.valueOf(User.count()));
+       users.add(newUser);
+       listSizeTextField.setText(String.valueOf(users.getSize()));
+       userTableInit();
+
+    }
+
+    public void userTableInit() {
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        ageColumn.setCellValueFactory(new PropertyValueFactory<>("age"));
+
+        usersTableView.setItems(getUserObservableList());
+    }
+
+    public ObservableList<User> getUserObservableList() {
+        List<User> userList = users.castToList();
+        return FXCollections.observableList(userList);
+    }
+
+    public void findUserButtonClick(){
+        int userNumber = Integer.parseInt(findUserByNumberTextField.getText());
+        User foundUser = users.get(userNumber);
+
+        foundNameTextField.setText(foundUser.getName());
+        foundAgeTextField.setText(String.valueOf(foundUser.getAge()));
+
+        findUserByNumberTextField.setText("");
+
+    }
+
 
     public void changeOnRussian() {
         Locale.setDefault(new Locale("ru"));
